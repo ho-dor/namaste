9cd4791c701aa49e1a9b28c06530657b1dd48730
Kunal Rai <kunalrai@Kunals-MacBook-Air.local>
2023-03-23 21:53:39 +0530

    update: lesson 2

--- App.js
+++ App.js
@@ -1,4 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
 const heading = React.createElement('h1', {id:'heading'}, "Hello World React");
 const nestedDOM = React.createElement('div', {}, React.createElement("div", {id:"parent"}, React.createElement("h1", {id:"child"}, "this is h1 tag")));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(nestedDOM);
+console.log(React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED)
