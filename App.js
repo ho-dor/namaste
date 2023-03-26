41cb98b95828622fd6a71d6b040742f2c24cb408
Kunal Rai <kunalrai@Kunals-MacBook-Air.local>
2023-03-26 11:51:54 +0530

    update: assignment lesson-3

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 const heading = React.createElement('h1', {id:'heading'}, "Hello World React");
 const nestedDOM = React.createElement('div', {}, React.createElement("div", {id:"parent"}, React.createElement("h1", {id:"child"}, "this is h1 tag")));
 
-
+const nestedHeader = React.createElement('div', {class:'title'}, React.createElement('h1', {}, React.createElement('h2', {}, React.createElement('h3', {}, 'Nested Header Component'))));
 
 const Heading2 = () => <h1>This is jsx</h1>;
 
@@ -18,8 +18,25 @@ const FirstFunctionalComponent = () => {
     )
 }
 
+const NestedHeaderJSX = () => {
+    return (
+        <div className='title'><h1><h2><h3>Nested header using JSX</h3></h2></h1></div>
+    )
+}
+
+const TopHeaderComponent = () => {
+    return (
+        <div style={{display: 'flex', justifyContent: 'space-between'}}>
+            <p>Logo</p>
+            <input placeholder='search'/>
+            {/* <img src='icon-user'/> */}
+            <p>User Icon</p>
+        </div>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<FirstFunctionalComponent/>);
+root.render(TopHeaderComponent());
 
 
 
