5ac22ca7711c5864fb2caf19c934ed5994c1a40d
Kunal Rai <kunalrai@Kunals-MacBook-Air.local>
2023-03-26 11:40:17 +0530

    update: jsx lesson-3

--- App.js
+++ App.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom/client'
 
 const heading = React.createElement('h1', {id:'heading'}, "Hello World React");
 const nestedDOM = React.createElement('div', {}, React.createElement("div", {id:"parent"}, React.createElement("h1", {id:"child"}, "this is h1 tag")));
+
+
+
+const Heading2 = () => <h1>This is jsx</h1>;
+
+const FirstFunctionalComponent = () => {
+    
+    return (
+        <>
+            <Heading2 />
+            <h1>This is a functional Component</h1>
+        </>
+    )
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(nestedDOM);
-console.log(React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED)
+root.render(<FirstFunctionalComponent/>);
+
+
+
